3d02b83948722f6362c884a88a6e397747f0df1e
Grigory Leonenko <falloff.th@gmail.com>
2014-02-20 21:00:06 +0600

    structure

---
+++ js/engine.js
@@ -0,0 +1,11 @@
+(function(){
+	window.onload = function(){
+		var game = document.getElementById('game');
+		context = game.getContext('2d');
+		context.fillStyle = "rgb(200,0,0)";
+        context.fillRect (10, 10, 55, 50);
+
+        context.fillStyle = "rgba(0, 0, 200, 0.5)";
+        context.fillRect (30, 30, 55, 50);
+	}
+})();
