ba3c3e3dc45d51ae9a7aec64aa84085bbd1e139a
Grigory Leonenko <falloff.th@gmail.com>
2014-02-21 19:26:05 +0600

    background animation

--- js/engine.js
+++ js/engine.js
@@ -1,11 +1,54 @@
-(function(){
-	window.onload = function(){
-		var game = document.getElementById('game');
-		context = game.getContext('2d');
-		context.fillStyle = "rgb(200,0,0)";
-        context.fillRect (10, 10, 55, 50);
+var ctx = null;
 
-        context.fillStyle = "rgba(0, 0, 200, 0.5)";
-        context.fillRect (30, 30, 55, 50);
-	}
+var road = {
+    src: 'img/road_texture.jpg',
+    frames: 60,
+    currentFrame: 0,
+    width: 500,
+    height: 500
+};
+
+var animHeight = road.height/road.frames;
+
+var img = new Image();
+img.addEventListener('load', function(){
+    init();
+});
+
+img.src = road.src;
+
+
+var requestAnimFrame = (function(){
+    return window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        };
 })();
+
+function init(){
+    ctx = document.getElementById('game').getContext('2d');
+    loop();
+};
+
+function loop(){
+    ctx.clearRect ( 0 , 0 , 500 , 500 );
+    drawRoad();
+    requestAnimFrame(loop);
+};
+
+function drawRoad(){
+
+    if(road.currentFrame == 0){
+        ctx.drawImage(img, 0, 0);
+        road.currentFrame++;
+    } else if(road.currentFrame > 0){
+        ctx.drawImage(img, 0, (road.frames - road.currentFrame) * animHeight, road.width, road.currentFrame * animHeight, 0, 0, road.width, road.currentFrame * animHeight);
+        ctx.drawImage(img, 0, road.currentFrame * animHeight);
+        if(road.currentFrame == road.frames){road.currentFrame = 0} else {road.currentFrame++};
+    };
+};
+
